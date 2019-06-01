c63742aaea8c2a9955fade90795b3de980284417
Monark <agarwalmonark@gmail.com>
2019-06-01 15:45:50 +0530

    Issue resolved for duplicate  subscription key

--- linux/startSolarIPFS.sh
+++ linux/startSolarIPFS.sh
@@ -1,4 +1,6 @@
 kill $(ps aux | grep 'watch.sh' | awk '{print $2}')
+value=`cat $HOME/.ipfs/subscription.key`
+curl 'https://solarbankersico.com/SolarNode_Service.svc/JSON/Configure/'$value;
 chmod -R 777 subscription.sh
 chmod -R 777 watch.sh
 nohup ./watch.sh >/dev/null &
